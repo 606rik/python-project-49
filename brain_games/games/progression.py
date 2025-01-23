eea38543bb705fe586da5461ff2558d74d43c917
606rik <ev.bob.team@gmail.com>
2025-01-23 22:56:56 +0700

    Пофикшена игра progression

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -16,7 +16,7 @@ def get_question_and_answer():
     hidden_index = random.randint(0, length - 1)
 
     correct_answer = str(progression[hidden_index])
-    progression[hidden_index] = " .. "  # Скрытое число
+    progression[hidden_index] = ".."  # Скрытое число
 
     question = " ".join(map(str, progression))
     return question, correct_answer
